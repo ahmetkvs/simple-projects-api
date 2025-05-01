84a1c34a4b1ec424819a7e6f1b85937c0493ad74
Ahmet Kavas <ahmetpkavas@gmail.com>
2025-05-01 10:09:38 +0300

    Update index.js
    new project added

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ const projects = [
     image:
       "https://res.cloudinary.com/dotjbmwdl/image/upload/v1743034867/tikit-project-screenshot.png",
   },
+  {
+    id: 3,
+    name: "WebWares",
+    descriptionEn:
+      "As the capstone project for my Frontend Bootcamp at Workintech, I developed this e-commerce platform to practically apply a modern React tech stack. This intensive, hands-on experience was crucial in navigating the complexities of building a real-world application, allowing me to learn and refine my skills through each stage of development. Reflecting on this project, I recognize the significant learning curve and envision the potential for even greater optimization and polish with additional time and experience.",
+    descriptionTr:
+      "Workintech'teki Frontend Bootcamp'imin bitirme projesi olarak geliştirdiğim bu e-ticaret platformu, modern bir React teknoloji yelpazesini pratik olarak uygulama fırsatı sundu. Bu yoğun ve uygulamalı deneyim, gerçek dünya ölçeğinde bir uygulama geliştirmenin karmaşıklıklarını anlamam ve her geliştirme aşamasında becerilerimi öğrenip iyileştirmem açısından kritik öneme sahipti. Bu projeye dönüp baktığımda, kaydettiğim önemli öğrenme sürecini görüyor ve ek zaman ve deneyimle çok daha iyi bir hale getirme potansiyelini hayal edebiliyorum.",
+    techStack: ["Redux", "Redux-Thunk", "React"],
+    githubUrl: "https://github.com/ahmetkvs/webwares",
+    deployUrl: "https://https://webwares.vercel.app/",
+    image:
+      "https://res.cloudinary.com/dotjbmwdl/image/upload/v1746083247/D4AADD94-5E77-4C80-AA2F-1A3248B37647_ighqcq.png",
+  },
 ];
 
 router.get("/projects", (req, res) => {
